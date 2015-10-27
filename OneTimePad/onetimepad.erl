017493cacbbd0628160377107915ebdb53bef519
Pejvan Beigui <pejvan@gmail.com>
2015-10-27 22:30:14 +0100

    fixed the key length, as the expected length was in bytes and not bits. added a console output in hex, as in the course

--- OneTimePad/onetimepad.erl
+++ OneTimePad/onetimepad.erl
@@ -10,16 +10,16 @@
 -spec genKey(KeyLength::integer())  -> GeneratedKey::string().
 genKey(KeyLength) when is_integer(KeyLength) ->
   GeneratedInts = [round(random:uniform()) || _ <- lists:seq(1, KeyLength)],
-  %GeneratedKey = iolist_to_binary(GeneratedInts),
   GeneratedKey =  << <<X:1>> || X <- GeneratedInts >>,
-  io:format("GeneratedKey as bitstring:~n"),
-  io:write(GeneratedKey),
-  io:format("~n"),
-  io:format("GeneratedKey as human readable format: ~p~n", [ GeneratedInts ]),
-  io:format("size of Generated key in bytes: ~w and in bits: ~w ~n", [byte_size(GeneratedKey), bit_size(GeneratedKey)] ).
+  %io:format("GeneratedKey as bitstring:~n"),
+  %io:write(GeneratedKey),
+  io:format("GeneratedKey as Hex string:~n0x~s~n", [[io_lib:format("~2.16.0B",[X]) || <<X:8>> <= GeneratedKey ]]),
+  %io:format("GeneratedKey as human readable format:~n~p~n", [ GeneratedInts ]),
+  %io:format("size of Generated key in bytes: ~w and in bits: ~w ~n", [byte_size(GeneratedKey), bit_size(GeneratedKey)] )
+  io:format("~n").
 
 
 test() ->
-  genKey(12).
+  genKey(12*8).
 
 
